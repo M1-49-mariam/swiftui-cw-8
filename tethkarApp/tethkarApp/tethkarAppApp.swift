1e24a12aed2274c9733e4de170d92f8311813de5
M1-50-mariam <76564921+M1-50-mariam@users.noreply.github.com>
2020-12-29 21:14:19 +0300

    Create tethkarAppApp.swift

---
+++ tethkarApp/tethkarApp/tethkarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  tethkarAppApp.swift
+//  tethkarApp
+//
+//  Created by Mariam2 on 12/29/20.
+//
+
+import SwiftUI
+
+@main
+struct tethkarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
